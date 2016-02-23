41e09f52700867dc7fbe7db33f1bfee1f60ffa6d
Cojomax99 <cojomax99@gmail.com>
2016-02-22 19:32:06 -0500

    Added beginnings of tools

---
+++ src/main/java/net/tropicraft/item/ItemTropicraftHoe.java
@@ -0,0 +1,12 @@
+package net.tropicraft.item;
+
+import net.minecraft.item.ItemHoe;
+
+public class ItemTropicraftHoe extends ItemHoe {
+
+	public ItemTropicraftHoe(ToolMaterial material) {
+		super(material);
+		// set creative tab
+	}
+
+}
--- src/main/java/net/tropicraft/registry/ItemRegistry.java
+++ src/main/java/net/tropicraft/registry/ItemRegistry.java
@@ -1,9 +1,12 @@
 package net.tropicraft.registry;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.tropicraft.Tropicraft;
 import net.tropicraft.item.ItemTropicraftFood;
+import net.tropicraft.item.ItemTropicraftHoe;
 import net.tropicraft.item.ItemTropicsOre;
 
 public class ItemRegistry extends TropicraftRegistry {
@@ -13,6 +16,14 @@ public class ItemRegistry extends TropicraftRegistry {
 	
 	// Food
 	public static Item grapefruit, lemon, lime, orange;
+	
+    public static ToolMaterial materialZirconTools = EnumHelper.addToolMaterial("zircon", 2, 200, 6.5F, 2.5F, 14);
+    public static ToolMaterial materialEudialyteTools = EnumHelper.addToolMaterial("eudialyte", 2, 750, 5.5F, 1.5F, 14);
+    public static ToolMaterial materialZirconiumTools = EnumHelper.addToolMaterial("zirconium", 3, 1800, 8.5F, 3.5F, 10);
+	
+	// Tools
+    public static Item hoeEudialyte;
+    public static Item hoeZircon;
 
 	/**
 	 * Register items in preInit
@@ -26,6 +37,9 @@ public class ItemRegistry extends TropicraftRegistry {
 		lemon = registerItem(new ItemTropicraftFood(2, 0.2F), "lemon");
 		lime = registerItem(new ItemTropicraftFood(2, 0.2F), "lime");
 		orange = registerItem(new ItemTropicraftFood(2, 0.2F), "orange");
+		
+		hoeEudialyte = registerItem(new ItemTropicraftHoe(materialEudialyteTools), "hoe_eudialyte");
+		hoeZircon = registerItem(new ItemTropicraftHoe(materialZirconTools), "hoe_zircon");
 	}
 	
 	private static Item registerItem(Item item, String name) {
