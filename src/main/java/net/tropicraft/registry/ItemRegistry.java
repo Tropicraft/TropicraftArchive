7625a43f5341f684af615e66a2f51668c9dfc450
Cojomax99 <cojomax99@gmail.com>
2016-02-22 19:17:24 -0500

    Add tropifruits

--- src/main/java/net/tropicraft/registry/ItemRegistry.java
+++ src/main/java/net/tropicraft/registry/ItemRegistry.java
@@ -3,12 +3,17 @@ package net.tropicraft.registry;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.tropicraft.Tropicraft;
+import net.tropicraft.item.ItemTropicraftFood;
 import net.tropicraft.item.ItemTropicsOre;
 
 public class ItemRegistry extends TropicraftRegistry {
 
 	// Ore gems
 	public static Item azurite, eudialyte, zircon;
+	
+	// Food
+	public static Item grapefruit, lemon, lime, orange;
+
 	/**
 	 * Register items in preInit
 	 */
@@ -16,6 +21,11 @@ public class ItemRegistry extends TropicraftRegistry {
 		azurite = registerItem(new ItemTropicsOre(), "azurite");
 		eudialyte = registerItem(new ItemTropicsOre(), "eudialyte");
 		zircon = registerItem(new ItemTropicsOre(), "zircon");
+		
+		grapefruit = registerItem(new ItemTropicraftFood(2, 0.2F), "grapefruit");
+		lemon = registerItem(new ItemTropicraftFood(2, 0.2F), "lemon");
+		lime = registerItem(new ItemTropicraftFood(2, 0.2F), "lime");
+		orange = registerItem(new ItemTropicraftFood(2, 0.2F), "orange");
 	}
 	
 	private static Item registerItem(Item item, String name) {
