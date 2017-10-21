78d329c8f205563a23ed402bddc06300f8ed7465
Corosauce <cordonfreeman@gmail.com>
2017-10-20 22:40:08 -0400

    json models hate fun

--- src/main/java/net/tropicraft/core/common/block/BlockTropicraftStairs.java
+++ src/main/java/net/tropicraft/core/common/block/BlockTropicraftStairs.java
@@ -2,11 +2,19 @@ package net.tropicraft.core.common.block;
 
 import net.minecraft.block.BlockStairs;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockTropicraftStairs extends BlockStairs {
 
 	public BlockTropicraftStairs(IBlockState modelState) {
 		super(modelState);
+	}
+
+	@SideOnly(Side.CLIENT)
+	public BlockRenderLayer getBlockLayer() {
+		return BlockRenderLayer.CUTOUT;
 	}
 
 }
